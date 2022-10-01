3d49616e60a25a158623e4fa1e80f1c55b727e2d
Dmitry G <dm.golodnikov@gmail.com>
2022-10-01 17:32:37 +0000

    HW02102022

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,6 +30,7 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
 	public void welcomerSaysHunterCheck(){
 		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
